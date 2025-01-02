374720d65f7bf60f164925496d187b42eca2cd7b
hyenicil <57847728+hyenicil@users.noreply.github.com>
2025-01-02 05:53:20 +0300

    Tasarimin home page kismi tamamlandi.

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className='relative'>
+            <div className='top-0 w-full py-14 bg-[#3A4750]'></div>
+            <div className='flex bg-[#EEEEEE] h-[90vh] absolute top-6 left-6 w-full'>
+                <div className='left w-[30%] bg-[#F6C90E] h-full]'>
+                    
+                <div className='w-full'>
+                    
+
+                </div>
+                </div>
+                <div className='right'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
